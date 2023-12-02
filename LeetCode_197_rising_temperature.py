e98f6286d589d540bb6f2a69e0e74cbc2c11211c
Nitij Taneja <124418733+nitij-taneja@users.noreply.github.com>
2023-12-03 00:27:17 +0530

    Create LeetCode_197_rising_temperature.py

---
+++ LeetCode_197_rising_temperature.py
@@ -0,0 +1,52 @@
+'''Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).'''
+
+import pandas as pd
+import datetime as dt
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+  result=weather.sort_values(by='recordDate',ascending=True)
+  result['td']=result['temperature'].diff()
+  result['dd']=result['recordDate'].diff().dt.days
+  return result[(result['td']>0) & (result['dd']==1)][['id']]
