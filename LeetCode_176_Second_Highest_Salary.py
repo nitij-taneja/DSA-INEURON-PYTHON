91811c5b372b2149a3b97494c0d32fe547231065
Nitij Taneja <124418733+nitij-taneja@users.noreply.github.com>
2023-12-07 21:39:03 +0530

    Create LeetCode_176_Second_Highest_Salary.py

---
+++ LeetCode_176_Second_Highest_Salary.py
@@ -0,0 +1,60 @@
+'''
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+'''
+
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    max_salry=employee['salary'].max()
+    second_highest_salary=employee[employee['salary']<max_salry]['salary'].max()
+    result=pd.DataFrame({'SecondHighestSalary':[int(second_highest_salary) if pd.notnull(second_highest_salary) else None]})
+    return result
